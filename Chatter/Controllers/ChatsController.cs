5a8574281e245dce17bf0f13c6de4575352a2c50
Your Name <you@example.com>
2017-08-17 14:49:06 -0400

    edited controller & index to show messages & usernames

--- Chatter/Controllers/ChatsController.cs
+++ Chatter/Controllers/ChatsController.cs
@@ -8,9 +8,12 @@ using System.Web;
 using System.Web.Mvc;
 using Chatter.Models;
 using Newtonsoft.Json;
+using Microsoft.AspNet.Identity;
 
 namespace Chatter.Controllers
 {
+    [Authorize]
+    [RequireHttps]
     public class ChatsController : Controller
     {
         private ChatterEntities1 db = new ChatterEntities1();
@@ -21,14 +24,54 @@ namespace Chatter.Controllers
             var chats = db.Chats.Include(c => c.AspNetUser);
             return View(chats.ToList());
         }
- 
+
         //Create a new method called TestJson. It returns a C# object called JsonResult
         public JsonResult TestJson()
         {
-            string jsonTest = "{ \"firstName\": \"Bob\", \"lastName\": \"Sauce\", \"children\": [{\"firstName\": \"Barbie\", \"age\": 19 },{\"firstName\": \"Ron\", \"age\": null }] }";
-            return Json(jsonTest, JsonRequestBehavior.AllowGet);
+            //string jsonTest = "{ \"firstName\": \"Bob\", \"lastName\": \"Sauce\", \"children\": [{\"firstName\": \"Barbie\", \"age\": 19 },{\"firstName\": \"Ron\", \"age\": null }] }";
+            //return Json(jsonTest, JsonRequestBehavior.AllowGet);
+
+            //can use Linqer to help get this but trial period expired on class computer:
+            var chats = from Chats in db.Chats
+                        orderby
+                        Chats.Timestamp descending
+                        select new
+                        {
+                            Chats.AspNetUser.UserName,
+                            Chats.Message
+                        };
+            var output = JsonConvert.SerializeObject(chats.ToList());
+            return Json(output, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult PostChats([Bind(Include = "Message")] Chat chat)
+        {
+            //we need to fill out information for our model so we can easily insert it.
+            //We need the following for our model / db. The user, the message and a timestamp.
+            //Since we're using entity framework, all of the Chat table's fields are in a C# model.
+            //We work through that (keeps everything in sync) and the model is what is used to update the database
+            //So it's a bit different than adding information directly to the database itself.
+
+            //We already have the message. It was passed to us via AJAX-> JSON vals from the View
+
+            //Let's add the Timestamp field's value
+            chat.Timestamp = DateTime.Now;
+            //Now, since we have a foreign key join on the asp.netuser table, the 2 models in EF (AspNetUser and Chat) reference each other
+            //Because of this, we need to get a complete user object, not just the userID 
+            string currentUserId = User.Identity.GetUserId();
+            chat.AspNetUser = db.AspNetUsers.FirstOrDefault(x => x.Id == currentUserId);
+
+            if (ModelState.IsValid)
+            {
+                db.Chats.Add(chat);
+                db.SaveChanges();
+            }
+            return new JsonResult() { Data = JsonConvert.SerializeObject(chat.ID), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
         
+
+
         // GET: Chats/Details/5
         public ActionResult Details(int? id)
         {
